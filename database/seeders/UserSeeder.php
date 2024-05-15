7f32928866e9ef5af2fc8b91d8aeb5e02232d069
Kabin <kevin.grg.msg@gmail.com>
2024-05-15 09:52:55 +0100

    Created User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker= Faker::create();
+        for($i=1; $i<=50; $i++){
+            $user= new User;
+            $user->name     =   $faker->name;
+            $user->email    =   $faker->email;
+            $user->password =   $faker->password;
+            $user->role     = "user";
+            $user->save();
+        }
+    }
+}
